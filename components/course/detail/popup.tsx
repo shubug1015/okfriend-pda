ade80035a3790736134747b8f6c53d95a0e98fa4
Viaduct <alien@reeddst.com>
2022-06-07 19:19:49 +0900

    Let course details page go back when survey form is submitted.

--- components/course/detail/popup.tsx
+++ components/course/detail/popup.tsx
@@ -7,6 +7,7 @@ import { FieldErrors, useForm } from 'react-hook-form';
 import useSWR from 'swr';
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
 import { useLocale } from '@libs/client/useLocale';
+import { useRouter } from 'next/router';
 
 interface IForm {
   Q1: string;
@@ -54,11 +55,15 @@ export default function Popup({ id, closePopup }: IProps) {
   } = useForm<IForm>({
     mode: 'onChange',
   });
+
+  const router = useRouter();
+
   const onValid = async (data: IForm) => {
     try {
       await surveyApi.courseSurvey(id, { ...data }, myData?.token as string);
       alert('제출이 완료되었습니다');
       closePopup();
+      router.back();
     } catch {
       alert('Error');
     }
